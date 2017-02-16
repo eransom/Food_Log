890255e99158dd6bff6fb0e773bc3062f853bd12
Eric Ransom <ericransom@Erics-MacBook-Pro.local>
2017-02-16 17:06:51 -0500

    updated monthlyView

--- src/components/monthlyView.js
+++ src/components/monthlyView.js
@@ -17,38 +17,6 @@ class MonthlyView extends Component {
     }
   }
 
-signUp(e) {
-      e.preventDefault()
-      var password = this.password.value
-    if (password.length < 6) {
-     alert('Password must be 6 or more characters')
-    } else {
-     base.createUser({
-        email: this.email.value,
-        password: this.password.value
-      }, this.authStateChanged.bind(this))
-      console.log(this.email.value)
-    }
-      this.email.value = ''
-      this.password.value = ''
-    }
-
-
-    authStateChanged (error, user) {
-      if (error) {
-        console.log(error)
-        alert('wrong password')
-      } else if (user) {
-        console.log(user.email)
-            this.setState({
-              userName: user.email
-            })
-        hashHistory.push("/foodSearch")
-        }
-    }
-
-
-
     render() {
 
       return (
