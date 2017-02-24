41ef0c434f8973285298cef2927b93ce74934202
Eric Ransom <ericransom@Erics-MBP.cfl.rr.com>
2017-02-24 15:50:05 -0500

    changes to budget in firebase

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import App from './App';
-import FoodSearch from './components/foodSearch.js'
 import SignUp from './components/signUp.js'
 import SignIn from './components/signIn.js'
+import FoodSearch from './components/foodSearch.js'
+import CalorieBudget from './components/calorieBudget.js'
 import MonthlyView from './components/monthlyView.js'
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -14,6 +15,7 @@ ReactDOM.render(
     <Route path="/" component={App} >
       <IndexRoute component={SignIn}></IndexRoute>
       <Route path="signUp" component={SignUp}/>
+      <Route path="calorieBudget" component={CalorieBudget}/>
       <Route path="foodSearch" component={FoodSearch}/>
       <Route path="monthlyView" component={MonthlyView}/>
     </Route>
