6e53190978ec26630d9cc300f300f21b6232d7a5
Eric Ransom <ericransom@Erics-MBP.cfl.rr.com>
2017-03-05 01:10:45 -0500

    setting up day picker to mealList

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,35 +1,81 @@
 import React, { Component } from 'react'
 import DayPicker from 'react-day-picker'
-import "../DayPicker.css"
+import '../DayPicker.css'
+import base from '../config'
+import Moment from 'moment'
+
 
 class Calendar extends Component {
+
   constructor(props) {
-    super(props)
-    this.handleDayClick = this.handleDayClick.bind(this)
+    super(props);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.showCurrentDate = this.showCurrentDate.bind(this);
   }
 
   state = {
-    selectedDay: null,
+    value: Moment().format('MMMM Do, YYYY'), // The value of the input field
+    month: new Date(), // The month to display in the calendar
+  };
+
+  showCurrentDate() {
+    this.daypicker.showMonth(this.state.month);
   }
 
-  handleDayClick(day, {selected}) {
+  handleInputChange(e) {
+    const { value } = e.target;
 
+    // Change the current month only if the value entered by the user
+    // is a valid date, according to the `MMMM Do, YYYY` format
+    if (Moment(value, 'MMMM Do, YYYY', true).isValid()) {
+      this.setState({
+        month: Moment(value, 'MMMM Do, YYYY').toDate(),
+        value,
+      }, this.showCurrentDate);
+    } else {
+      this.setState({ value }, this.showCurrentDate);
+    }
+  }
+
+  handleDayClick(day) {
     this.setState({
-      selectedDay: day
-    })
+      value: Moment(day).format('MMMM Do, YYYY'),
+      month: day,
+    });
   }
 
-  render(){
-    const { selectedDay } = this.state
-    return(
+  selectDateToMealList(){
+    const formattedDT = Moment().format('MMMM Do, YYYY')
+    base.fetch(`users/${this.props.uid}/meals/${formattedDT}`, {
+      context: this,
+    })
+    console.log(this.state.value)
+  }
+
+  render() {
+    const selectedDay = Moment(this.state.value, 'MMMM Do, YYYY', true).toDate();
+    this.selectDateToMealList()
+    return (
       <div id="calendar">
+        <p>
+          <input
+            type="text"
+            value={ this.state.value }
+            placeholder="YYYY-MM-DD"
+            onChange={ this.handleInputChange }
+            onFocus={ this.showCurrentDate }
+          />
+        </p>
         <DayPicker
-          onDayClick={ this.handleDayClick }
+          ref={ el => this.daypicker = el }
+          initialMonth={ this.state.month }
           selectedDays={ selectedDay }
+          onDayClick={ this.handleDayClick }
         />
       </div>
-    )
+    );
   }
-}
 
+}
 export default Calendar
